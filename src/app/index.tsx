95daf99be5147a78a161c91ea062748af4f8cbe3
ageu <guianovo3214@email.com>
2025-04-04 21:01:49 -0300

    primeiro commit

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View ,Text } from "react-native";
+
+export default function Index () {
+    return (
+         <View> 
+            
+            <Text  style={{ color: "red", fontSize: 40, }} >Hellow, Elon Musk!!</Text>
+        
+         </View>
+    );
+}
